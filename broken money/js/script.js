4f7809ecdec36012bf662247e1c9c4a88a9d2d76
rijwanulalam <rijwanul007@gmail.com>
2023-12-06 21:39:36 +0600

    money master solved

--- broken money/js/script.js
+++ broken money/js/script.js
@@ -1,9 +1,9 @@
 const calculateExpense = () => {
-  const income = document.querySelector("income").value;
-  const food = document.querySelector("food").value;
-  const rent = document.querySelector("rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
-
+  const income = document.querySelector("#income").value;
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes = document.querySelector("#clothes").value;
+  // console.log(typeof parseInt(income))
   if (
     income < 0 ||
     income == "" ||
@@ -14,17 +14,17 @@ const calculateExpense = () => {
     clothes < 0 ||
     clothes == ""
   ) {
-    alert("Inputs must be positive numbers");
+    alert("Inputs must be a positive numbers");
     return;
   }
   // calculate expense
   const expense =
     parseInt(food) + parseInt(rent) + parseInt(clothes);
-
+    console.log(expense)
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  const balance = parseInt(income) - expense;
   //   validate income
-  if (expense > income.value) {
+  if (expense > parseInt(income)) {
     alert("Expenses cannot be more than income");
   } else {
     // view total expense and balance
@@ -35,23 +35,26 @@ const calculateExpense = () => {
 
 const calculateSavings = () => {
   // calculate saving amount
+  const income = document.querySelector("#income").value;
   const savePercentage = document.getElementById("save").value;
-//   Validate saving percentage value
+  //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
+    return;
   }
-  const savingAmount = (savePercentage / 100) + income;
-
-  // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
+  const savingAmount = (parseInt(savePercentage) / 100) * income;
+  // calculate remaining balance
   const remainingBalance = balance - savingAmount;
-
+  
+  
   //   validate saving amount
   if (savingAmount > balance) {
     alert("SavingAmount is greater than balance");
   } else {
     // view saving amount and remaining balance
-    document.getElementById("saving-amount").innerText = savingAmount;
-    document.getElementById("remaining-balance").innerText = remainingBalance;
+    document.getElementById("saving-amount").innerText = savingAmount.toFixed(2);
+    // document.getElementById("remaining-balance").innerText = remainingBalance;
+    document.getElementById("remaining-balance").innerText = remainingBalance.toFixed(2);
   }
 };

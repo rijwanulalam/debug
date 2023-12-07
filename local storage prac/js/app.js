47d1392d56aaae0485200480d5782fc032877a4c
rijwanulalam <rijwanul007@gmail.com>
2023-12-07 14:50:39 +0600

    save to local storage

--- local storage prac/js/app.js
+++ local storage prac/js/app.js
@@ -1,1 +1,31 @@
-console.log("connected");
+document.getElementById("btn-product").addEventListener("click", function(){
+    const inputNameField = document.getElementById("input-name").value;
+    const inputValueField = document.getElementById("input-value").value;
+    document.getElementById("input-name").value = '';
+    document.getElementById("input-value").value = '';
+    displayProducts(inputNameField, inputValueField)
+    saveProductToLocalStorage(inputNameField, inputValueField)
+})
+
+const displayProducts = (inputNameField, inputValueField) => {
+    const ul = document.getElementById("list-container");
+    const li = document.createElement("li");
+    li.innerHTML = `${inputNameField} : ${inputValueField}`;
+    ul.appendChild(li);
+}
+
+const getStoredShoppingCart = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem("cart");
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem("cart", cartStringified);
+}

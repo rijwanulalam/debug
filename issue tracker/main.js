320cbc78ecfcd9277b1fd22a984bd7a15c01f351
rijwanulalam <rijwanul007@gmail.com>
2023-12-08 14:56:46 +0600

    status changed successfully to local storage

--- issue tracker/main.js
+++ issue tracker/main.js
@@ -7,36 +7,46 @@ function submitIssue(e) {
   const assignedTo = getInputValue('issueAssignedTo');
   const id = Math.floor(Math.random()*100000000) + '';
   const status = 'Open';
-
-  const issue = { id, description, severity, assignedTo, status };
+  
   let issues = [];
+  const issue = { id, description, severity, assignedTo, status };
   if (localStorage.getItem('issues')){
     issues = JSON.parse(localStorage.getItem('issues'));
   }
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
-
+  
   document.getElementById('issueInputForm').reset();
   fetchIssues();
   e.preventDefault();
 }
 
 const closeIssue = id => {
+  // if(id){
+  //   console.log(id)
+  // }else{
+  //   console.log("no id")
+  // }
+  // console.log(typeof id)
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  // console.log(issues)
+  const currentIssue = issues.find(issue => parseInt(issue.id) === id );
+  // console.log(currentIssue)
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
 
 const deleteIssue = id => {
+
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issues.id !== id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
 }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
+  // console.log(issues)
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
 
@@ -49,7 +59,7 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
